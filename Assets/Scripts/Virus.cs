3da1ce648a573f521e0fea588a8507568bf7f32e
Olusegun Sojobi <sojobiolusegun@gmail.com>
2020-05-08 12:04:25 +0100

    Spawn and move virus towards player

---
+++ Assets/Scripts/Virus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+    public float rotationAmount = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * Vector3.forward * 2;
+        transform.Rotate( Time.deltaTime * Vector3.right * rotationAmount);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ontrigger");
+        if(other.tag == "DeathPoint")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/VirusSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusSpawner : MonoBehaviour
+{
+	public GameObject[] virusStrains;
+    public BoxCollider spawnVolume;
+	public float beat;
+	private float timer;	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > beat)
+        {
+            Vector3 minPoint = spawnVolume.bounds.min;
+            Vector3 maxPoint = spawnVolume.bounds.max;
+            Vector3 spawnPoint = new Vector3(
+                Random.Range(minPoint.x, maxPoint.x),
+                Random.Range(minPoint.y, maxPoint.y),
+                Random.Range(minPoint.z, maxPoint.z));
+            GameObject cube = Instantiate(virusStrains[Random.Range(0, virusStrains.Length - 1)], spawnPoint, Random.rotation);
+            //cube.transform.localPosition = Vector3.zero;
+            //cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+            timer -= beat;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
