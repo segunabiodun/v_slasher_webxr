2807b71e2548191188c7f87765caa71e7fca6acc
Olusegun Sojobi <sojobiolusegun@gmail.com>
2020-05-15 19:38:39 +0100

    Set all lights to baked, Use baked lighting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        Application.targetFrameRate = 60;
+        //Application.targetFrameRate = 60;
     }
 
     public void StartGame()
